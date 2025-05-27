c3d839101aef7bf204e7f04b3b2fb4a1f62ec2cc
Ado <adonaylester85@gmail.com>
2025-05-26 19:37:31 -0600

    Actualizar rpg-depositar.js

--- plugins/rpg-depositar.js
+++ plugins/rpg-depositar.js
@@ -8,16 +8,16 @@ if (args[0] == 'all') {
 let count = parseInt(user.coin)
 user.coin -= count * 1
 user.bank += count * 1
-await m.reply(`${emoji} Depositaste *${count} ${moneda}* en el banco, ya no podran robartelo.`)
+await m.reply(`> ${emoji} Depositaste *${count} ${moneda}* en el banco, ya no podran robartelo.`)
 return !0
 }
 if (!Number(args[0])) return m.reply(`${emoji2} Debes depositar una cantidad válida.\n> Ejemplo 1 » *#d 25000*\n> Ejemplo 2 » *#d all*`)
 let count = parseInt(args[0])
 if (!user.coin) return m.reply(`${emoji2} No tienes suficientes *${moneda}* la Cartera.`)
-if (user.coin < count) return m.reply(`${emoji2} Solo tienes *${user.coin} ${moneda}* en la Cartera.`)
+if (user.coin < count) return m.reply(`${emoji2} Namas tienes *${user.coin} ${moneda}* en la cartera.`)
 user.coin -= count * 1
 user.bank += count * 1
-await m.reply(`${emoji} Depositaste *${count} ${moneda}* en el banco, ya no podran robartelo.`)}
+await m.reply(`> ${emoji} Depositaste *${count} ${moneda}* en el banco, ya no podran robartelo.`)}
 
 handler.help = ['depositar']
 handler.tags = ['rpg']
