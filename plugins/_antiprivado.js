e649925b595013cebba64ee980d8394ad65e91c4
Ado <adonaylester85@gmail.com>
2025-05-26 19:57:52 -0600

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,14 +1,23 @@
 export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
-  if (m.isBaileys && m.fromMe) return !0;
-  if (m.isGroup) return !1;
-  if (!m.message) return !0;
-  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
+  if (m.isBaileys && m.fromMe) return true;
+  if (m.isGroup) return false;
+  if (!m.message) return true;
+  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return true;
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
-if (m.chat === '120363416409380841@newsletter') return !0
+  if (m.chat === '120363416409380841@newsletter') return true;
+
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`${emoji} Hola @${m.sender.split`@`[0]}, mi creador a desactivado los comandos en los chats privados el cual serás bloqueado, si quieres usar los comandos del bot te invito a que te unas al grupo principal del bot.\n\n${gp1}`, false, {mentions: [m.sender]});
+    await m.reply(
+      `> 𝗛𝗼𝗹𝗮 @${m.sender.split`@`[0]} ✨\n\n` +  // Fuente negrita sans serif
+      `> Por ahora los comandos solo están activos en grupos.\n` +
+      `> Si quieres usar la bot, únete a este grupo:\n` +
+      `> \`https://chat.whatsapp.com/GBcSWbfm3JO1HhmbdbnrsH\`\n\n` +  // Fuente monospace
+      `> Te esperamos 💖`,
+      false,
+      { mentions: [m.sender] }
+    );
     await this.updateBlockStatus(m.chat, 'block');
   }
-  return !1;
+  return false;
 }
