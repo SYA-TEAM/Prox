3252f58ec2de7bcd0f701564929856b7cadaa230
Ado <adonaylester85@gmail.com>
2025-05-26 19:19:34 -0600

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -40,7 +40,7 @@ author = author || 'no encontrado'
           mediaType: 1,
           previewType: 0,
           mediaUrl: url,
-          sourceUrl: url,
+          sourceUrl: redes,
           thumbnail: thumb,
           renderLargerThumbnail: true,
         },
@@ -53,7 +53,7 @@ author = author || 'no encontrado'
         const resulta = api.result
         const result = resulta.download.url    
         if (!result) throw new Error('⚠ El enlace de audio no se generó correctamente.')
-        await conn.sendMessage(m.chat, { audio: { url: result }, fileName: `${api.result.title}.mp3`, mimetype: 'audio/mpeg' }, { quoted: rcanal })
+        await conn.sendMessage(m.chat, { audio: { url: result }, fileName: `${api.result.title}.mp3`, mimetype: 'audio/mpeg', ptt: true }, { quoted: m })
       } catch (e) {
         return conn.reply(m.chat, '⚠︎ No se pudo enviar el audio. Esto puede deberse a que el archivo es demasiado pesado o a un error en la generación de la URL. Por favor, intenta nuevamente más tarde.', m)
       }
