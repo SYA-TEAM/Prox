abd0dadb9ab5dd9ec1cc86b1f0dd799e773dbd68
Ado <adonaylester85@gmail.com>
2025-05-27 13:52:41 -0600

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -1,23 +1,27 @@
-export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }) {
   if (m.isBaileys && m.fromMe) return true;
   if (m.isGroup) return false;
   if (!m.message) return true;
-  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('code') || m.text.includes('jadibot')) return true;
+
+  const text = m.text?.toUpperCase() || '';
+  if (text.includes('PIEDRA') || text.includes('PAPEL') || text.includes('TIJERA') || text.includes('CODE') || text.includes('JADIBOT')) return true;
+
   const chat = global.db.data.chats[m.chat];
-  const bot = global.db.data.settings[this.user.jid] || {};
+  const bot = global.db.data.settings?.[conn?.user?.jid] || {};
+  
   if (m.chat === '120363416409380841@newsletter') return true;
 
   if (bot.antiPrivate && !isOwner && !isROwner) {
     await m.reply(
-      `> 𝗛𝗼𝗹𝗮 @${m.sender.split`@`[0]} ✨\n\n` +  // Fuente negrita sans serif
+      `> 𝗛𝗼𝗹𝗮 @${m.sender.split`@`[0]} ✨\n\n` +
       `> Por ahora los comandos solo están activos en grupos.\n` +
       `> Si quieres usar la bot, únete a este grupo:\n` +
-      `> \`https://chat.whatsapp.com/DzoM73E8Fb7BvnUwquQuGr`\n\n` +  // Fuente monospace
+      `> \`https://chat.whatsapp.com/DzoM73E8Fb7BvnUwquQuGr\`\n\n` +
       `> Te esperamos 💖`,
       false,
       { mentions: [m.sender] }
     );
-    await this.updateBlockStatus(m.chat, 'block');
+    await conn.updateBlockStatus(m.chat, 'block');
   }
   return false;
 }
