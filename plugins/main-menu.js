98ac9400eda8256d89e026f7e91af78fd437e45e
Ado <adonaylester85@gmail.com>
2025-05-26 18:26:09 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -8,7 +8,7 @@ let handler = async (m, { conn, args }) => {
     let totalCommands = Object.values(global.plugins).filter((v) => v.help && v.tags).length
 
     let txt = `
-> *❒❅ Hola! Soy*  *${botname}* *❅❒*
+> *Holis Soy* *${botname}*
 Aquí tienes la lista de comandos :3
 ╭┈ ▼
 │❀ 𝖴𝗌𝖾𝗋 » @${userId.split('@')[0]}
