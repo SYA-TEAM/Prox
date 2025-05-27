dc2ecd445c3b484e2b944aa3b6f89eaf6f7612a5
Ado <adonaylester85@gmail.com>
2025-05-27 15:17:36 -0600

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -72,7 +72,7 @@ author = author || 'no encontrado'
     return m.reply(`⚠︎ Ocurrió un error: ${error}`)
   }
 }
-handler.command = handler.help = ['play', 'yta', 'ytmp3', 'play2', 'playaudio', 'mp4']
+handler.command = handler.help = ['play', 'yta', 'ytmp3', 'playaudio', 'mp4']
 handler.tags = ['descargas']
 handler.group = true
 
