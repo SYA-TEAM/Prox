8bce5e111fb5a3a41f9dc93c08a2a51065776eb0
Ado <adonaylester85@gmail.com>
2025-06-01 16:04:15 -0600

    Update handler.js

--- handler.js
+++ handler.js
@@ -487,7 +487,7 @@ await plugin.after.call(this, m, extra)
 console.error(e)
 }}
 if (m.coin)
-conn.reply(m.chat, `❮✦❯ Utilizaste ${+m.coin} ${moneda}`, m)
+conn.reply(m.chat, `❮ 🎫 ❯ Utilizaste ${+m.coin} ${moneda}`, m, rcanal)
 }
 break
 }}
