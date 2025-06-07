548471fab2165ed7f211d615f0e331fd456a5db9
Ado <adonaylester85@gmail.com>
2025-06-07 12:13:03 -0600

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -2,39 +2,40 @@
 
 import fetch from 'node-fetch';
 
-let handler = async(m, { conn, usedPrefix, command, text }) => {
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+  if (!text) return m.reply(`✿ Ingresa un texto para buscar en YouTube\n> *Ejemplo:* ${usedPrefix + command} Bella Wolfine`);
 
-if (!text) return m.reply(`✿ Ingresa Un Texto Para Buscar En Youtube\n> *Ejemplo:* ${usedPrefix + command}Bella Wolfine`);
+  await m.react('🕒'); // Reacción mientras busca
 
-try {
-let api = await (await fetch(`https://delirius-apiofc.vercel.app/search/ytsearch?q=${text}`)).json();
+  try {
+    let api = await (await fetch(`https://delirius-apiofc.vercel.app/search/ytsearch?q=${encodeURIComponent(text)}`)).json();
+    let result = api.data[0];
 
-let results = api.data[0];
+    let txt = `「✿」 ✦ *𝖳𝗂𝗍𝗎𝗅𝗈 =* ${result.title}
+✦ *𝖣𝗎𝗋𝖺𝖼𝗂𝗈́𝗇 =* ${result.duration}
+✦ *𝖫𝗂𝗇𝗄 =* ${result.url}
+✦ *𝖯𝗎𝖻𝗅𝗂𝖼𝖺𝖽𝗈 =* ${result.publishedAt}`;
 
-let txt = `「✿」 ✦ *𝖳𝗂𝗍𝗎𝗅𝗈 =* ${results.title}\n✦ *𝖣𝗎𝗋𝖺𝖼𝗂𝗈́𝗇 =* ${results.duration}\n✦ *𝖫𝗂𝗇𝗄: =* ${results.url}\n✦ *𝖯𝗎𝖻𝗅𝗂𝖼𝖺𝖽𝗈 =* ${results.publishedAt}`;
-
-let img = results.image;
-
-conn.sendMessage(m.chat, { 
-        image: { url: img }, 
-        caption: txt, 
-        footer: dev, 
-        buttons: [
-            {
-                buttonId: `.ytmp4 ${results.url}`,
-                buttonText: { displayText: '❀ Descargar ❀' }
-            }
-        ],
-        viewOnce: true,
-        headerType: 4
+    await conn.sendMessage(m.chat, {
+      image: { url: result.image },
+      caption: txt,
+      footer: 'By Wirk para Anya',
+      buttons: [
+        {
+          buttonId: `.ytmp4 ${result.url}`,
+          buttonText: { displayText: '❀ Descargar ❀' }
+        }
+      ],
+      viewOnce: true,
+      headerType: 4
     }, { quoted: m });
 
-} catch (e) {
-m.reply(`Error: ${e.message}`);
-m.react('✖️');
+    await m.react('✅'); // Reacción al terminar
+  } catch (e) {
+    await m.react('✖️');
+    m.reply(`Error: ${e.message}`);
   }
-}
+};
 
 handler.command = ['play2', 'mp4'];
-
-export default handler
+export default handler;
