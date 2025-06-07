b03ba536713e3cc68770348c50b34366e3c8a0ab
Ado <adonaylester85@gmail.com>
2025-06-07 17:03:24 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -55,7 +55,7 @@ global.etiqueta = '✦ ᴡɪʀᴋ';
 global.moneda = '¥enes'
 global.welcom1 = '❍ Edita Con El Comando setwelcome'
 global.welcom2 = '❍ Edita Con El Comando setbye'
-global.banner = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742678744381.jpeg'
+global.banner = 'https://qu.ax/AbbjX.jpg'
 global.avatar = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742678797993.jpeg'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
