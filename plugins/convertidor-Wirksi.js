99f9a747da5480f91d0c91f596b05b855d82065e
Ado <adonaylester85@gmail.com>
2025-06-07 20:22:34 -0600

    Update convertidor-Wirksi.js

--- plugins/convertidor-Wirksi.js
+++ plugins/convertidor-Wirksi.js
@@ -36,7 +36,7 @@ let handler = async (m, { conn }) => {
 
 handler.help = ['maybox'];
 handler.tags = ['uploader'];
-handler.command = ['box'];
+handler.command = ['wirksibox', 'tourl'];
 export default handler;
 
 // --- Funciones auxiliares ---
