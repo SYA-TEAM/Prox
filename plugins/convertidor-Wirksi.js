c2d9f24c033002d119e7a758cf176cdf8738d63c
Ado <adonaylester85@gmail.com>
2025-06-07 20:16:12 -0600

    Update convertidor-Wirksi.js

--- plugins/convertidor-Wirksi.js
+++ plugins/convertidor-Wirksi.js
@@ -51,7 +51,7 @@ async function maybox(content, mime) {
   const filename = `${Date.now()}-${crypto.randomBytes(3).toString('hex')}.${ext}`;
   form.append('file', blob, filename);
 
-  const res = await fetch('https://wirksibox.onrender.com', {
+  const res = await fetch('https://wirksibox.onrender.com/api/upload', {
     method: 'POST',
     body: form,
     headers: {
